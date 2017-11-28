620e67e080176ac8442802d3b5f115139fb6f90d
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-11-28 23:13:18 +0200

    refuse connections from same ip

--- JavaScript/net-server.js
+++ JavaScript/net-server.js
@@ -2,73 +2,37 @@
 
 const net = require('net');
 
-// const sockets = new Set();
-// const server = net.createServer((socket) => {
-//   sockets.forEach((sckt) => {
-//     sckt.write(`Client ${socket.localAddress} connected\n`);
-//   });
-//   socket.setEncoding('utf8');
-//   socket.on('data', (data) => {
-//     sockets.forEach((sckt) => {
-//       sckt.write(`📨  ${socket.localAddress}: ` + data);
-//     });
-//   });
-//
-//   socket.on('end', () => {
-//     console.log(`Client ${socket.localAddress} disconnected`);
-//     sockets.delete(socket);
-//     sockets.forEach((sckt) => {
-//       sckt.write(`Client ${socket.localPort} disconnected\n`);
-//     });
-//   });
-//
-// });
-//
-// server.listen(8080);
-//
-// server.on('error', (err) => {
-//   throw err;
-// });
-//
-// console.log(server.address());
-//
-// server.on('connection', (socket) => {
-//   sockets.add(socket);
-//   console.log(`Client ${socket.localAddress} ${socket.remoteAddress} connected`);
-//   socket.write(`You are on server\nIP: ${socket.localAddress}\n`);
-// });
-
-
-const sockets = new Set();
+const sockets = new Map();
 const server = net.createServer((socket) => {
-  sockets.forEach((sckt) => {
-    sckt.write(`${socket.remotePort} connected\n`);
-  });
-  socket.setEncoding('utf8');
-  socket.on('data', (data) => {
-    sockets.forEach((sckt) => {
-      sckt.write(`📨  ${socket.remotePort}: ` + data);
-    });
-  });
+  const ip = socket.remoteAddress;
+  if (!sockets.has(ip)) {
+    sockets.set(ip, socket);
 
-  socket.on('end', () => {
-    console.log(`Client ${socket.remotePort} disconnected`);
-    sockets.delete(socket);
-    sockets.forEach((sckt) => {
-      sckt.write(`${socket.remotePort} disconnected\n`);
-    });
-  });
+    console.log(`Client ${ip} connected`);
+    socket.write(`You are on server\nIP: ${ip}\n`);
 
+    sockets.forEach((sckt) => {
+      sckt.write(`${socket.remoteAddress} connected\n`);
+    });
+
+    socket.setEncoding('utf8');
+    socket.on('data', (data) => {
+      sockets.forEach((sckt) => {
+        sckt.write(`📨  ${socket.remoteAddress}: ` + data);
+      });
+    });
+
+    socket.on('end', () => {
+      console.log(`Client ${socket.remoteAddress} disconnected`);
+      sockets.delete(ip);
+      sockets.forEach((sckt) => {
+        sckt.write(`${socket.remoteAddress} disconnected\n`);
+      });
+    });
+  } else socket.end();
 });
 
 server.listen(8080, '192.168.0.107');
-// console.log(server.address());
 server.on('error', (err) => {
   throw err;
-});
-
-server.on('connection', (socket) => {
-  sockets.add(socket);
-  console.log(`Client ${socket.remoteAddress} connected`);
-  socket.write(`You are on server\nRemote port: ${socket.remotePort}\n`);
 });
